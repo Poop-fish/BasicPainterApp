59fbf43979f2711b7ce464645015f2a9456aea47
GamerTag <justagamert4g@gmail.com>
2025-01-26 11:38:23 -0500

    Add files via upload
    Example code if some doesn't know how to use it

---
+++ Example.py
@@ -0,0 +1,14 @@
+from GT_imports import *
+from GT_Core_Management import *
+
+def Example():
+
+    root = tk.Tk()
+    
+    Paint_App = BasicPaint(root)
+    
+    root.mainloop()
+    
+if __name__ == "__main__":
+    Example()
+
