07751e8b073b2655eaabca836ccee7c0b7b8d5e4
GamerTag <justagamert4g@gmail.com>
2025-01-26 14:11:24 -0500

    Update GT_CustomStyle.py
    Style Update

--- GT_CustomStyle.py
+++ GT_CustomStyle.py
@@ -1,5 +1,13 @@
 from GT_imports import *
+
 def apply_styles(root):
     style = ttk.Style(root)
+    style.theme_use("alt")
+
+    #* \\ Frame Style \\
     style.configure("TFrame", relief="solid", borderwidth=2)
-    style.configure("TButton", relief="raised", padding=6, font=("Arial", 10))
+
+     #* \\ Button Style \\
+    style.configure("TButton", background="Gray", foreground="Black", font=("Arial", 14) ,relief="ridge", borderwidth=10)
+    style.map("TButton", background=[("active", "darkgray")])
+    
