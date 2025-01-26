1c08a9faa09f4f9939c0224fb3ef97326461ce6b
GamerTag <justagamert4g@gmail.com>
2025-01-26 14:20:33 -0500

    Update GT_imports.py
    organized better

--- GT_imports.py
+++ GT_imports.py
@@ -1,18 +1,9 @@
 # \\ tk and tkk imports \\ 
 import tkinter as tk
-
-from tkinter import colorchooser, ttk
-
-from tkinter import ttk
-
-from tkinter import colorchooser
-
-from tkinter import filedialog
-
+from tkinter import colorchooser, filedialog, ttk
 from tkinter import Canvas, Scale, HORIZONTAL
 
-
-# \\ Base imports \\
+# \\ Base imports \\ 
 from PIL import Image, ImageDraw 
 
 import math
