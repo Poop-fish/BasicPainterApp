29948d27726ea010711412255473e4ddddd27743
GamerTag <justagamert4g@gmail.com>
2025-01-26 13:24:21 -0500

    Update GT_Ui_Management.py
    update for spin buttons and added custom pallet pop up window

--- GT_Ui_Management.py
+++ GT_Ui_Management.py
@@ -18,6 +18,8 @@ def create_menu(app):
 
     app.root.config(menu=menu_bar)
 
+#!--------------------------------------------------------------------------------------------------------------------------------
+
 def create_sidebar(app):
     
     sidebar = ttk.Frame(app.root, width=200, height=600)
@@ -29,17 +31,26 @@ def create_sidebar(app):
         y = app.center_y + app.radius * math.sin(math.radians(angle))  
 
         button = tk.Button(
-            sidebar, width=4, height=2, command=lambda c=color: app.change_color(c)
+            sidebar, width=3, height=1, command=lambda c=color: app.change_color(c)
         )
         button.configure(background=color)
-        button.place(x=x - 20, y=y - 10)  
+        button.place(x=x - 5, y=y - 2)  
         app.color_buttons.append((button, angle)) 
 
     app.spin_buttons()
-
-    pick_color_button = ttk.Button(sidebar, text="Pick Color", command=app.pick_color)
+    
+    pick_color_button = ttk.Button(sidebar, text="Pre Built Color Palette", command=app.pick_color)
     pick_color_button.pack(pady=5, padx=5, fill="x")
-    app.custom_color_picker = CustomColorPicker(sidebar, app.change_color)
+    
+    app.custom_color_picker = CustomColorPicker(app.change_color)
+    custom_color_picker_button = ttk.Button(
+        sidebar,
+        text="Custom Color Palette",
+        command=app.custom_color_picker.open_picker 
+    )
+    custom_color_picker_button.pack(pady=5, padx=5, fill="x")
+
+#!--------------------------------------------------------------------------------------------------------------------------------
 
 def create_bottom_toolbar(app):
     toolbar = ttk.Frame(app.root, height=50)
@@ -60,6 +71,9 @@ def create_bottom_toolbar(app):
 
     clear_button = ttk.Button(toolbar, text="Clear", command=app.clear_canvas)
     clear_button.pack(side="right", padx=10)
+    
+#!--------------------------------------------------------------------------------------------------------------------------------
+
 def create_ui_elements(app):
     create_menu(app)
     create_sidebar(app)
