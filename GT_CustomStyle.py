30770001b1712ad81417f305cf5afbf33dbd9169
GamerTag <justagamert4g@gmail.com>
2025-01-26 19:40:11 -0500

    Update GT_CustomStyle.py
    changed style a bit

--- GT_CustomStyle.py
+++ GT_CustomStyle.py
@@ -1,13 +1,17 @@
 from GT_imports import *
-
 def apply_styles(root):
     style = ttk.Style(root)
     style.theme_use("alt")
-
-    #* \\ Frame Style \\
+    
     style.configure("TFrame", relief="solid", borderwidth=2)
 
      #* \\ Button Style \\
-    style.configure("TButton", background="Gray", foreground="Black", font=("Arial", 14) ,relief="ridge", borderwidth=10)
+    style.configure("TButton", background="#7f7f7f", foreground="Black", font=("Arial", 14) ,relief="ridge", borderwidth=10)
     style.map("TButton", background=[("active", "darkgray")])
     
+    style.configure("TScale", background="black", sliderlength=25, troughcolor="#7f7f7f")
+    
+        # * \\ Label Style \\ *
+    style.configure("TLabel", background="#7f7f7f", foreground="black", font=("Arial", 12, "bold"), anchor="center")
+    
+ 
