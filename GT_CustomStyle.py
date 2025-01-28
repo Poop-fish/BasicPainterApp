903a902bab21423285a4be0fc69bc08b6ca4fe0a
GamerTag <justagamert4g@gmail.com>
2025-01-27 22:27:02 -0500

    Update GT_CustomStyle.py
    New Update

--- GT_CustomStyle.py
+++ GT_CustomStyle.py
@@ -13,5 +13,23 @@ def apply_styles(root):
     
         # * \\ Label Style \\ *
     style.configure("TLabel", background="#7f7f7f", foreground="black", font=("Arial", 12, "bold"), anchor="center")
+        #* \\ Scrollbar Style \\
+    style.configure("TScrollbar", background="black", troughcolor="#444444", arrowcolor="#00db00" )
+        #* \\ Entry Widget Style \\
+    style.configure("TEntry", fieldbackground="#444444", foreground="#00db00", font=("Arial", 14))
     
- 
+    
+def custom_menu_styles(menu):
+    menu.configure(
+        bg="gray",
+        fg="black",
+        font=("Arial", 12),
+        relief="ridge",
+        borderwidth=3,
+        activebackground="#c8c8c8",
+        activeforeground="#1aff00",
+        disabledforeground="gray"
+    ) 
+
+    menu.bind("<Enter>", lambda event: menu.configure(bf="lightgray"))
+    menu.bind("<Leave>", lambda event: menu.configure(bf="darkgray"))
