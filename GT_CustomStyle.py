a68ea5d0c1e97b2b258ecd0c330f779a87cdb6a4
GamerTag <justagamert4g@gmail.com>
2025-01-29 13:55:12 -0500

    Update GT_CustomStyle.py
    New update

--- GT_CustomStyle.py
+++ GT_CustomStyle.py
@@ -5,20 +5,20 @@ def apply_styles(root):
     
     style.configure("TFrame", relief="solid", borderwidth=2)
 
-     #* \\ Button Style \\
+        #* \\ Button Style \\
     style.configure("TButton", background="#7f7f7f", foreground="Black", font=("Arial", 14) ,relief="ridge", borderwidth=10)
     style.map("TButton", background=[("active", "darkgray")])
-    
+        # * \\ Scale Style \\ 
     style.configure("TScale", background="black", sliderlength=25, troughcolor="#7f7f7f")
-    
-        # * \\ Label Style \\ *
+        # * \\ Label Style \\ 
     style.configure("TLabel", background="#7f7f7f", foreground="black", font=("Arial", 12, "bold"), anchor="center")
         #* \\ Scrollbar Style \\
-    style.configure("TScrollbar", background="black", troughcolor="#444444", arrowcolor="#00db00" )
+    style.configure("TScrollbar", background="black", troughcolor="#444444", arrowcolor="#00db00", )
         #* \\ Entry Widget Style \\
     style.configure("TEntry", fieldbackground="#444444", foreground="#00db00", font=("Arial", 14))
-    
-    
+
+
+  
 def custom_menu_styles(menu):
     menu.configure(
         bg="gray",
@@ -33,3 +33,12 @@ def custom_menu_styles(menu):
 
     menu.bind("<Enter>", lambda event: menu.configure(bf="lightgray"))
     menu.bind("<Leave>", lambda event: menu.configure(bf="darkgray"))
+
+#* \\ ttk.Label (TLabel) \\
+def toggle_flash(label, colors, index):
+    next_index = (index + 1) % len(colors)  # \\ Cycle through the colors
+    label.config(foreground=colors[index])
+    label.after(500, toggle_flash, label, colors, next_index)  #  \\ Call again after 500ms
+
+# style.configure("Flashing.TLabel", background="#7f7f7f", font=("Arial", 16, "bold"), anchor="center")
+
